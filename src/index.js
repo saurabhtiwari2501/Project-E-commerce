6ef3077fe257b1b706b828f9773e3c6dbbf07cb7
saurabhtiwari2501 <saurabhtiwari2501@gmail.com>
2022-10-10 16:27:55 +0530

    User

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const route = require('../src/routes/routes');
+const  mongoose  = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+
+
+mongoose.connect("mongodb+srv://functionup-cohert:2aZSPLpUOON7ZWA2@cluster0.sl0yd7n.mongodb.net/PRODUCTMANAGEMENT", {
+        useNewUrlParser: true
+    })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
